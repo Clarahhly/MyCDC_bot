1f2db54f54c7b6c97d0bb729f19d3bc6cbc773a8
Your Name <you@example.com>
2024-09-22 02:40:53 +1000

    initial commit

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from twocaptcha import TwoCaptcha
 
 
 # Your 2Captcha API Key
