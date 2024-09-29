693c39073f6be86cb0f29fd66f60db0216c3a82f
Your Name <you@example.com>
2024-09-29 20:39:31 +1000

    added requirements

--- src/save_cookies.py
+++ src/save_cookies.py
@@ -1,8 +1,6 @@
 import time
 import pickle
 import random
-import undetected_chromedriver as uc
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
